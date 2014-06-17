390fd8eea693c74a1590bf5c9471709df9ccbfbe
Brian Widhalm <bdwidhalm@gmail.com>
2014-06-17 06:42:07 -0600

    first commit

---
+++ Tiff2Pdf/Module1.vb
@@ -0,0 +1,58 @@
+﻿Imports System.IO
+Imports PdfSharp.Pdf
+Imports PdfSharp.Pdf.IO
+Imports PdfSharp.Drawing
+Imports PdfSharp.Drawing.XImage
+
+Module Module1
+    Dim tiff As New TiffImageSplitter
+
+    Sub Main()
+        Console.WriteLine("Enter path and name to list file: ")
+        Dim fileList = Console.ReadLine()
+        Dim line As String
+        Dim lineArr() As String
+        Dim pdf As New PdfDocument
+        Dim pdf2 As New PdfDocument
+        Try
+            If File.Exists(fileList) Then
+                Dim sr As New StreamReader(fileList)
+                Do While sr.Peek() <> -1
+                    line = sr.ReadLine
+                    lineArr = line.Split("|")
+                    'Console.WriteLine(lineArr(0))
+                    'Console.WriteLine(lineArr(1))
+                    Console.WriteLine(lineArr(2))
+
+                    Dim pageCount = tiff.getPageCount(lineArr(1))
+
+                    For i As Integer = 0 To pageCount - 1
+                        Dim page As New PdfPage
+                        Dim tiffImg As System.Drawing.Image
+                        tiffImg = tiff.getTiffImage(lineArr(1), i)
+                        Dim img = XImage.FromGdiPlusImage(tiffImg)
+
+                        page.Width = img.PointWidth
+                        page.Height = img.PointHeight
+                        pdf.Pages.Add(page)
+
+                        Dim xgr = XGraphics.FromPdfPage(pdf.Pages(i))
+
+                        xgr.DrawImage(img, 0, 0)
+                        Console.WriteLine("Page #" + (i + 1).ToString)
+                    Next
+
+                    pdf.Save(lineArr(2))
+                    pdf.Close()
+
+                Loop
+            Else
+                Console.WriteLine("File didn't exist!")
+            End If
+            'Console.ReadLine()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Module
---
+++ Tiff2Pdf/TiffImageSplitter.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class TiffImageSplitter
+    Public Function getPageCount(ByVal fileName As String) As Integer
+        Dim pageCount = -1
+        Try
+            Dim img = Bitmap.FromFile(fileName)
+            pageCount = img.GetFrameCount(FrameDimension.Page)
+            img.Dispose()
+        Catch ex As Exception
+            pageCount = 0
+        End Try
+
+        Return pageCount
+    End Function
+
+    'Retrieve a specific Page from a multi-page tiff image
+    Public Function getTiffImage(ByVal sourceFile As String, ByVal pageNumber As Integer) As Image
+        Dim returnImage As Image
+        Try
+            Dim sourceImage = Bitmap.FromFile(sourceFile)
+            returnImage = getTiffImage(sourceImage, pageNumber)
+            sourceImage.Dispose()
+        Catch ex As Exception
+            returnImage = Nothing
+        End Try
+        Return returnImage
+    End Function
+
+    Public Function getTiffImage(ByVal sourceImage As Image, ByVal pageNumber As Integer) As Image
+        Dim ms As MemoryStream
+        Dim returnImage As Image
+        Try
+            ms = New MemoryStream
+            Dim objGuid = sourceImage.FrameDimensionsList(0)
+            Dim objDimension = New FrameDimension(objGuid)
+            sourceImage.SelectActiveFrame(objDimension, pageNumber)
+            sourceImage.Save(ms, ImageFormat.Tiff)
+            returnImage = Image.FromStream(ms)
+        Catch ex As Exception
+            returnImage = Nothing
+        End Try
+        Return returnImage
+    End Function
+
+End Class
